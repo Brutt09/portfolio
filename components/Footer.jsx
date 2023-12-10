d29133c97e640958f155bca0e75052aad0ba356b
Brutt09 <leonleebrit@gmail.com>
2023-12-10 21:14:44 +0800

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,6 +5,13 @@ const Footer = () => {
         <footer className="w-full border-t border-[#33353F]">
             <div className="mb-14 w-full container mx-auto p-4 flex justify-between items-center text-slate-400">
                 <span className="">Britney</span>
+                <section className="text-white grid" id="herosection">
+                           
+                        <div>
+                            <img src="/Hero-Section.png" alt="About Me" width={450} height={450} style={{ borderRadius: "25px" }}/>
+                        </div>
+                    
+                </section>
                 <p>Copyright &copy; All rights reserved</p>
             </div>
         </footer>
